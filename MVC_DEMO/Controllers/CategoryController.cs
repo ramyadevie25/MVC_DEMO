27cd9dc1890bff62aac09a5e9326a0e68177aa89
Ramya Devie <ramya@soulchiinternational.com>
2024-10-16 13:05:59 -0400

    mvc demo initial commit

---
+++ MVC_DEMO/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC_DEMO.Data;
+using MVC_DEMO.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVC_DEMO.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Category> catlist = _db.Categories.ToList();
+            return View(catlist);
+        }
+    }
+}
+
---
+++ MVC_DEMO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC_DEMO.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
